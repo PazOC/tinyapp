3ea25f6842ca5b3fa028e236a70655de43216692
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-09-01 20:47:32 +0000

    Modified routes to display Username

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
+
+
 
 function generateRandomString(length = 6) {
   return Math.random().toString(32).substr(2, 6);
@@ -16,6 +19,10 @@ const urlDatabase = {
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
+
+// go to views and find those that render _header
+// find end points that render those views and pass username inside templatevars object.
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -43,18 +50,28 @@ app.get("/fetch", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => { 
   const shortURL = req.params.shortURL
   const longURL = urlDatabase[shortURL]
-  const templateVars = { shortURL, longURL };
+  const templateVars = { 
+    shortURL, 
+    longURL,
+    username: req.cookies["username"]
+   };
   res.render("urls_show", templateVars);
 });
 
@@ -86,5 +103,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username)
   res.redirect("/urls")
 })
